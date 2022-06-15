b4c5a875b7e74b7a5ce6cb22c2b797cb86920133
imRedz <vls.sidorow@gmail.com>
2022-06-15 20:35:17 +0300

    update

--- Main/Main/index.js
+++ Main/Main/index.js
@@ -13,26 +13,28 @@ export default function MainMain(props) {
   return (
     <>
       <section className={style.main}>
-        <div className="background desktop">
-          <Image
-            src={props.image}
-            layout="fill"
-            objectFit="cover"
-            objectPosition={props.image_objectPosition}
-            quality={90}
-            priority={true}
-          />
-        </div>
-        <div className="background mobile">
-          <Image
-            src={props.image_responsive}
-            layout="fill"
-            objectFit={props.image_responsive_objectFit}
-            objectPosition={props.image_responsive_objectPosition}
-            quality={90}
-            priority={true}
-          />
-        </div>
+        {props.image ?
+          <div className="background desktop">
+            <Image
+              src={props.image}
+              layout="fill"
+              objectFit="cover"
+              objectPosition={props.image_objectPosition}
+              quality={90}
+              priority={true}
+            />
+          </div> : ""}
+        {props.image_responsive ?
+          <div className="background mobile">
+            <Image
+              src={props.image_responsive}
+              layout="fill"
+              objectFit={props.image_responsive_objectFit}
+              objectPosition={props.image_responsive_objectPosition}
+              quality={90}
+              priority={true}
+            />
+          </div> : ""}
         <div className={`${style.content} ${contentTheme[props.theme ? props.theme : "light"]}`}>
           <Fade direction="left" delay={800} triggerOnce>
             <h1 className={style.title}>
