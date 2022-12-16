d880cf59f04727e404eab66fd8220cf6c07af86d
Vlad Rey <vls.sidorow@gmail.com>
2022-12-16 17:12:08 +0200

    updated Main page modules

--- Main/Main/newMain.js
+++ Main/Main/newMain.js
@@ -1,16 +1,10 @@
-import Image from "next/image"
-import style from "./style.module.scss"
-
-const contentTheme = {
-    "light": style.content_light,
-    "dark": style.content_dark
-}
-
+import Image from "next/image";
+import style from "./style.module.scss";
 
 export default function NewMainMain(props) {
     return (
         <section className={style.main}>
-            {props.image ?
+            {props.image && (
                 <div className="background desktop">
                     <Image
                         src={props.image}
@@ -20,8 +14,9 @@ export default function NewMainMain(props) {
                         quality={90}
                         priority={true}
                     />
-                </div> : ""}
-            {props.image_responsive ?
+                </div>
+            )}
+            {props.image_responsive && (
                 <div className="background mobile">
                     <Image
                         src={props.image_responsive}
@@ -31,19 +26,14 @@ export default function NewMainMain(props) {
                         quality={90}
                         priority={true}
                     />
-                </div> : ""}
-            <div className={`${style.content} ${contentTheme[props.theme ? props.theme : "light"]}`}>
-                {/* <Fade direction="right" delay={800} triggerOnce> */}
-                {/* <div className={style.content_align_right}> */}
-                <h1 className={`${style.title} fade-right-animation`} style={{ whiteSpace: "pre-line" }}>
+                </div>
+            )}
+            <div className={style.content}>
+                <h1 className={`${style.title} fade-right-animation`}>
                     {props.title}
                 </h1>
-                <p className={style.text}>
-                    {props.text}
-                </p>
-                {/* </div> */}
-                {/* </Fade> */}
+                <p className={style.text}>{props.text}</p>
             </div>
         </section>
-    )
+    );
 }
