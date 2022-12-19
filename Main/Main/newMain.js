176c22edf7eb42d141038bf7ec72384ab13f97d7
netronic-dev <dev.netronic@gmail.com>
2022-12-19 19:09:53 +0200

    change main banner, add button

--- Main/Main/newMain.js
+++ Main/Main/newMain.js
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import style from "./style.module.scss";
+import { Fade } from "react-awesome-reveal"
+import { FillButton, Button } from "../../../lt-modules/Buttons"
 
 export default function NewMainMain(props) {
     return (
@@ -32,7 +34,28 @@ export default function NewMainMain(props) {
                 <h1 className={`${style.title} fade-right-animation`}>
                     {props.title}
                 </h1>
-                <p className={style.text}>{props.text}</p>
+                <p className={`${style.text} fade-right-animation`}>{props.text}</p>
+                <div className={`${style.buttons} fade-right-animation`}>
+                    <Button
+                        type="catalog"
+                        text={props.buttonText}
+                        style="blueBlackMainBannerWithGirl"
+                    />
+                    {props.eventButtonLink ?
+                        <Fade delay={1500} triggerOnce>
+                            <div>
+                                <Link href={props.eventButtonLink ? props.eventButtonLink : "/"}>
+                                    <a>
+                                        <FillButton
+                                            className={style.event_button}
+                                            style="whiteBlack"
+                                            text={props.eventButtonText}
+                                        />
+                                    </a>
+                                </Link>
+                            </div>
+                        </Fade> : ""}
+                </div>
             </div>
         </section>
     );
