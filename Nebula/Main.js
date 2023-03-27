6ae506c1ac2b41ce3195f3bb253f6afe383ad3d9
artemyatsenko91 <aliens199@ukr.net>
2023-03-27 19:41:14 +0300

    Add Nebula folder

---
+++ Nebula/Main.js
@@ -0,0 +1,11 @@
+import style from './style.module.scss';
+
+const Main = () => {
+    return (
+        <section className={style.main}>
+            <h1 className={style.main_title}>Nebula</h1>
+        </section>
+    );
+};
+
+export default Main;
