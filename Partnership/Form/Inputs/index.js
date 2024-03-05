bbeb09d458c30b656b4f4967dbe3f54549c9e8ce
artemyatsenko91 <aliens199@ukr.net>
2024-03-05 18:53:30 +0200

    Fix

--- Partnership/Form/Inputs/index.js
+++ Partnership/Form/Inputs/index.js
@@ -1,24 +1,24 @@
-import style from './style.module.scss';
-import ReactGA from 'react-ga4';
-import axios from 'axios';
+import style from "./style.module.scss";
+import ReactGA from "react-ga4";
+import axios from "axios";
 
-import { useFormik } from 'formik';
-import { useRouter } from 'next/router';
-import { useInView } from 'react-hook-inview';
-import Link from 'next/link';
-import { useDispatch } from 'react-redux';
-import { setUserData } from '../../../../store/actions/userData';
+import { useFormik } from "formik";
+import { useRouter } from "next/router";
+import { useInView } from "react-hook-inview";
+import Link from "next/link";
+import { useDispatch } from "react-redux";
+import { setUserData } from "../../../../store/actions/userData";
 
-import { useValidation } from '../../../../context/ValidationProvider';
-import { postData } from '../../../../lt-modules/functions/postData.ts';
-import { useGAEvents } from '../../../../context/GAEventsProvider';
+import { useValidation } from "../../../../context/ValidationProvider";
+import { postData } from "../../../../lt-modules/functions/postData.ts";
+import { useGAEvents } from "../../../../context/GAEventsProvider";
 
 const inputsLandTheme = {
     default: style.input_land,
     light: style.input_land_light,
 };
 
-export function InputsWName (props) {
+export function InputsWName(props) {
     const validate = useValidation();
     const router = useRouter();
     const dispatch = useDispatch();
@@ -26,46 +26,50 @@ export function InputsWName (props) {
 
     const formik = useFormik({
         initialValues: {
-            name: '',
-            email: '',
-            phone: '',
-            agreement: '',
+            name: "",
+            email: "",
+            phone: "",
+            agreement: "",
         },
         validate,
         onSubmit: (values) => {
             dispatch(setUserData(values.name));
             const options = {
-                method: 'POST',
+                method: "POST",
                 url: `https://api.netronic.net/send-email`,
                 headers: {
-                    'content-type': 'application/json',
+                    "content-type": "application/json",
                 },
-                data: { email: values.email, fromName: props.fromName, letterId: props.letterId },
+                data: {
+                    email: values.email,
+                    fromName: props.fromName,
+                    letterId: props.letterId,
+                },
             };
             axios
                 .request(options)
                 .then(console.log)
                 .then(
                     postData(
-                        data,
+                        values,
                         props.destinationURL,
                         props.orderName,
                         props.lang,
                         window.location.href,
-                        router.query,
-                    ),
+                        router.query
+                    )
                 )
-                .then(router.push('/thanks-call'))
                 .catch(console.log);
-            ReactGA.event('generate_lead', {
-                category: 'form',
-                action: 'submit',
+            ReactGA.event("generate_lead", {
+                category: "form",
+                action: "submit",
             });
+            router.push("/thanks-call");
         },
     });
 
-    function onAgreementChange () {
-        formik.setFieldValue('agreement', !formik.values.agreement);
+    function onAgreementChange() {
+        formik.setFieldValue("agreement", !formik.values.agreement);
     }
 
     const [ref, isVisible] = useInView({
@@ -74,64 +78,62 @@ export function InputsWName (props) {
 
     return (
         <div className={style.input_land_out}>
-            <form
-                onSubmit={formik.handleSubmit}
-                className='form_submit_land'
-            >
+            <form onSubmit={formik.handleSubmit} className="form_submit_land">
                 <div className={style.content}>
                     <div className={style.input_out__outer}>
                         <div className={style.input_out}>
                             <input
-                                className={formik.errors.name ? `${style.input} ${style.input_error}` : style.input}
-                                name='name'
-                                maxLength='30'
+                                className={
+                                    formik.errors.name
+                                        ? `${style.input} ${style.input_error}`
+                                        : style.input
+                                }
+                                name="name"
+                                maxLength="30"
                                 onChange={formik.handleChange}
                                 value={formik.values.name}
-                                placeholder={
-                                    props.namePlaceholder || 'Name *'
-                                }
+                                placeholder={props.namePlaceholder || "Name *"}
                             />
                         </div>
-                        <div className={style.error}>
-                            {formik.errors.name}
-                        </div>
+                        <div className={style.error}>{formik.errors.name}</div>
                     </div>
                     <div className={style.input_out__outer}>
                         <div className={style.input_out}>
                             <input
-                                className={formik.errors.email ? `${style.input} ${style.input_error}` : style.input}
+                                className={
+                                    formik.errors.email
+                                        ? `${style.input} ${style.input_error}`
+                                        : style.input
+                                }
                                 onChange={formik.handleChange}
                                 value={formik.values.email}
-                                maxLength='40'
-                                name='email'
-                                type='email'
-                                placeholder={
-                                    props.placeholder || 'Email *'
-                                }
+                                maxLength="40"
+                                name="email"
+                                type="email"
+                                placeholder={props.placeholder || "Email *"}
                             />
                         </div>
-                        <div className={style.error}>
-                            {formik.errors.email}
-                        </div>
+                        <div className={style.error}>{formik.errors.email}</div>
                     </div>
                     <div className={style.input_out__outer}>
                         <div className={style.input_out}>
                             <input
-                                className={formik.errors.phone ? `${style.input} ${style.input_error}` : style.input}
-                                type='tel'
-                                name='phone'
-                                maxLength='30'
+                                className={
+                                    formik.errors.phone
+                                        ? `${style.input} ${style.input_error}`
+                                        : style.input
+                                }
+                                type="tel"
+                                name="phone"
+                                maxLength="30"
                                 onChange={formik.handleChange}
                                 value={formik.values.phone}
                                 placeholder={
-                                    props.callPlaceholder ||
-                                    'Phone number *'
+                                    props.callPlaceholder || "Phone number *"
                                 }
                             />
                         </div>
-                        <div className={style.error}>
-                            {formik.errors.phone}
-                        </div>
+                        <div className={style.error}>{formik.errors.phone}</div>
                     </div>
                 </div>
                 <Agreement
@@ -154,7 +156,7 @@ export function InputsWName (props) {
     );
 }
 
-function Agreement (props) {
+function Agreement(props) {
     return (
         <div className={style.agreement__outer}>
             <div className={style.agreement}>
@@ -171,11 +173,11 @@ function Agreement (props) {
                 <p className={style.agreement__text}>
                     <span onClick={props.onAgreementChange}>
                         {props.agreementText ||
-                            'I agree with conditions of the processing and use'}
-                    </span>{' '}
-                    <Link href='/privacy-policy'>
+                            "I agree with conditions of the processing and use"}
+                    </span>{" "}
+                    <Link href="/privacy-policy">
                         <a>
-                            {props.agreementSpanText || 'of my personal data'}
+                            {props.agreementSpanText || "of my personal data"}
                         </a>
                     </Link>
                 </p>
@@ -186,8 +188,8 @@ function Agreement (props) {
 }
 
 const dotIcon = (
-    <svg width='20' height='20' viewBox='0 0 20 20' fill='none'>
-        <circle cx='10' cy='10' r='10' fill='#383838' />
-        <circle className={style.dot} cx='10' cy='10' r='8' fill='#F1F4F6' />
+    <svg width="20" height="20" viewBox="0 0 20 20" fill="none">
+        <circle cx="10" cy="10" r="10" fill="#383838" />
+        <circle className={style.dot} cx="10" cy="10" r="8" fill="#F1F4F6" />
     </svg>
 );
