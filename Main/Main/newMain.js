eca1603c579760ac6e6777d8dfda32874b07f9b5
Albina Anistratenko <albuska2013@gmail.com>
2024-11-25 11:02:03 +0200

    delete main slider

--- Main/Main/newMain.js
+++ Main/Main/newMain.js
@@ -56,8 +56,8 @@ const settings = {
 export default function NewMainMain(props) {
   return (
     <>
-      <Slider {...settings} className={style.customSlider}>
-        <section>
+      {/* <Slider {...settings} className={style.customSlider}> */}
+      {/* <section>
           <BannerSlide
             logoImg="/blog/iaapa-expo-europe-2024/logo.png"
             registerLink="/blog/iaapa-orlando-2024/#register"
@@ -85,60 +85,60 @@ export default function NewMainMain(props) {
             stand="#4478"
             buttonText="Register Now"
           />
-        </section>
-        <section className={style.main}>
-          {props.image && (
-            <div className="background desktop">
-              <Image
-                src={props.image}
-                layout="fill"
-                objectFit="cover"
-                objectPosition={props.image_objectPosition}
-                quality={90}
-                priority={true}
-                alt="image"
-              />
-            </div>
-          )}
-          {props.image_responsive && (
-            <div className="background mobile">
-              <Image
-                src={props.image_responsive}
-                layout="fill"
-                objectFit={props.image_responsive_objectFit}
-                objectPosition={props.image_responsive_objectPosition}
-                quality={90}
-                priority={true}
-                alt="image"
-              />
-            </div>
-          )}
-          <div className={style.content}>
-            <h1 className={`${style.title} fade-right-animation`}>
-              {props.title}
-            </h1>
-            <p className={`${style.text} fade-right-animation`}>{props.text}</p>
-            <div className={`${style.buttons} fade-right-animation`}>
-              <Button
-                type="catalog"
-                text={props.buttonText}
-                style="blueBlackMainBannerWithGirl"
-              />
-              {props.eventButtonLink && (
-                <div>
-                  <a href={props.eventButtonLink}>
-                    <FillButton
-                      className={style.event_button}
-                      style="whiteBlack"
-                      text={props.eventButtonText}
-                    />
-                  </a>
-                </div>
-              )}
-            </div>
+        </section> */}
+      <section className={style.main}>
+        {props.image && (
+          <div className="background desktop">
+            <Image
+              src={props.image}
+              layout="fill"
+              objectFit="cover"
+              objectPosition={props.image_objectPosition}
+              quality={90}
+              priority={true}
+              alt="image"
+            />
           </div>
-        </section>
-      </Slider>
+        )}
+        {props.image_responsive && (
+          <div className="background mobile">
+            <Image
+              src={props.image_responsive}
+              layout="fill"
+              objectFit={props.image_responsive_objectFit}
+              objectPosition={props.image_responsive_objectPosition}
+              quality={90}
+              priority={true}
+              alt="image"
+            />
+          </div>
+        )}
+        <div className={style.content}>
+          <h1 className={`${style.title} fade-right-animation`}>
+            {props.title}
+          </h1>
+          <p className={`${style.text} fade-right-animation`}>{props.text}</p>
+          <div className={`${style.buttons} fade-right-animation`}>
+            <Button
+              type="catalog"
+              text={props.buttonText}
+              style="blueBlackMainBannerWithGirl"
+            />
+            {props.eventButtonLink && (
+              <div>
+                <a href={props.eventButtonLink}>
+                  <FillButton
+                    className={style.event_button}
+                    style="whiteBlack"
+                    text={props.eventButtonText}
+                  />
+                </a>
+              </div>
+            )}
+          </div>
+        </div>
+      </section>
+      {/* </Slider> */}
     </>
   );
 }
