fb6dd24fa945e9beda8d6330d6111b81edff7090
Albina Anistratenko <albuska2013@gmail.com>
2024-12-19 13:23:55 +0200

    add christmas banner

--- Main/Main/newMain.js
+++ Main/Main/newMain.js
@@ -39,8 +39,8 @@ const settings = {
   dotsClass: `slick-dots ${style.dots}`,
   nextArrow: <SampleNextArrow />,
   prevArrow: <SamplePrevArrow />,
-  // autoplay: true,
-  // autoplaySpeed: 3000,
+  autoplay: true,
+  autoplaySpeed: 3000,
   responsive: [
     {
       breakpoint: 743,
@@ -55,61 +55,61 @@ const settings = {
 export default function NewMainMain(props) {
   return (
     <>
-      {/* <Slider {...settings} className={style.customSlider}>
-        <Banner /> */}
-      <section className={style.main}>
-        {props.image && (
-          <div className="background desktop">
-            <Image
-              src={props.image}
-              layout="fill"
-              objectFit="cover"
-              objectPosition={props.image_objectPosition}
-              quality={90}
-              priority={true}
-              alt="image"
-            />
+      <Slider {...settings} className={style.customSlider}>
+        <Banner />
+        <section className={style.main}>
+          {props.image && (
+            <div className="background desktop">
+              <Image
+                src={props.image}
+                layout="fill"
+                objectFit="cover"
+                objectPosition={props.image_objectPosition}
+                quality={90}
+                priority={true}
+                alt="image"
+              />
+            </div>
+          )}
+          {props.image_responsive && (
+            <div className="background mobile">
+              <Image
+                src={props.image_responsive}
+                layout="fill"
+                objectFit={props.image_responsive_objectFit}
+                objectPosition={props.image_responsive_objectPosition}
+                quality={90}
+                priority={true}
+                alt="image"
+              />
+            </div>
+          )}
+          <div className={style.content}>
+            <h1 className={`${style.title} fade-right-animation`}>
+              {props.title}
+            </h1>
+            <p className={`${style.text} fade-right-animation`}>{props.text}</p>
+            <div className={`${style.buttons} fade-right-animation`}>
+              <Button
+                type="catalog"
+                text={props.buttonText}
+                style="blueBlackMainBannerWithGirl"
+              />
+              {props.eventButtonLink && (
+                <div>
+                  <a href={props.eventButtonLink}>
+                    <FillButton
+                      className={style.event_button}
+                      style="whiteBlack"
+                      text={props.eventButtonText}
+                    />
+                  </a>
+                </div>
+              )}
+            </div>
           </div>
-        )}
-        {props.image_responsive && (
-          <div className="background mobile">
-            <Image
-              src={props.image_responsive}
-              layout="fill"
-              objectFit={props.image_responsive_objectFit}
-              objectPosition={props.image_responsive_objectPosition}
-              quality={90}
-              priority={true}
-              alt="image"
-            />
-          </div>
-        )}
-        <div className={style.content}>
-          <h1 className={`${style.title} fade-right-animation`}>
-            {props.title}
-          </h1>
-          <p className={`${style.text} fade-right-animation`}>{props.text}</p>
-          <div className={`${style.buttons} fade-right-animation`}>
-            <Button
-              type="catalog"
-              text={props.buttonText}
-              style="blueBlackMainBannerWithGirl"
-            />
-            {props.eventButtonLink && (
-              <div>
-                <a href={props.eventButtonLink}>
-                  <FillButton
-                    className={style.event_button}
-                    style="whiteBlack"
-                    text={props.eventButtonText}
-                  />
-                </a>
-              </div>
-            )}
-          </div>
-        </div>
-      </section>
-      {/* </Slider> */}
+        </section>
+      </Slider>
     </>
   );
 }
