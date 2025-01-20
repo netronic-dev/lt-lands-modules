29a465a390baf383b573f3c57836e16e819aa0d4
albuska <albuska2013@gmail.com>
2025-01-20 09:30:19 +0200

    deleted christmas banner

--- Main/Main/newMain.js
+++ Main/Main/newMain.js
@@ -54,62 +54,60 @@ const settings = {
 
 export default function NewMainMain(props) {
   return (
-    <>
-      <Slider {...settings} className={style.customSlider}>
-        <Banner />
-        <section className={style.main}>
-          {props.image && (
-            <div className="background desktop">
-              <Image
-                src={props.image}
-                layout="fill"
-                objectFit="cover"
-                objectPosition={props.image_objectPosition}
-                quality={90}
-                priority={true}
-                alt="image"
-              />
+    // <>
+    //   <Slider {...settings} className={style.customSlider}>
+    //     <Banner />
+    <section className={style.main}>
+      {props.image && (
+        <div className="background desktop">
+          <Image
+            src={props.image}
+            layout="fill"
+            objectFit="cover"
+            objectPosition={props.image_objectPosition}
+            quality={90}
+            priority={true}
+            alt="image"
+          />
+        </div>
+      )}
+      {props.image_responsive && (
+        <div className="background mobile">
+          <Image
+            src={props.image_responsive}
+            layout="fill"
+            objectFit={props.image_responsive_objectFit}
+            objectPosition={props.image_responsive_objectPosition}
+            quality={90}
+            priority={true}
+            alt="image"
+          />
+        </div>
+      )}
+      <div className={style.content}>
+        <h1 className={`${style.title} fade-right-animation`}>{props.title}</h1>
+        <p className={`${style.text} fade-right-animation`}>{props.text}</p>
+        <div className={`${style.buttons} fade-right-animation`}>
+          <Button
+            type="catalog"
+            text={props.buttonText}
+            style="blueBlackMainBannerWithGirl"
+          />
+          {props.eventButtonLink && (
+            <div>
+              <a href={props.eventButtonLink}>
+                <FillButton
+                  className={style.event_button}
+                  style="whiteBlack"
+                  text={props.eventButtonText}
+                />
+              </a>
             </div>
           )}
-          {props.image_responsive && (
-            <div className="background mobile">
-              <Image
-                src={props.image_responsive}
-                layout="fill"
-                objectFit={props.image_responsive_objectFit}
-                objectPosition={props.image_responsive_objectPosition}
-                quality={90}
-                priority={true}
-                alt="image"
-              />
-            </div>
-          )}
-          <div className={style.content}>
-            <h1 className={`${style.title} fade-right-animation`}>
-              {props.title}
-            </h1>
-            <p className={`${style.text} fade-right-animation`}>{props.text}</p>
-            <div className={`${style.buttons} fade-right-animation`}>
-              <Button
-                type="catalog"
-                text={props.buttonText}
-                style="blueBlackMainBannerWithGirl"
-              />
-              {props.eventButtonLink && (
-                <div>
-                  <a href={props.eventButtonLink}>
-                    <FillButton
-                      className={style.event_button}
-                      style="whiteBlack"
-                      text={props.eventButtonText}
-                    />
-                  </a>
-                </div>
-              )}
-            </div>
-          </div>
-        </section>
-      </Slider>
-    </>
+        </div>
+      </div>
+    </section>
+    /* </Slider>
+    </> */
   );
 }
