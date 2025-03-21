6f00f2cc04b7f29342b537432b01e70543c82421
albuska <albuska2013@gmail.com>
2025-03-21 09:06:31 +0200

    delete slider with banner exhibition Las-Vegas

--- Main/Main/newMain.js
+++ Main/Main/newMain.js
@@ -1,61 +1,61 @@
 import Image from "next/image";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+// import Slider from "react-slick";
+// import "slick-carousel/slick/slick.css";
+// import "slick-carousel/slick/slick-theme.css";
 import style from "./style.module.scss";
 import { FillButton, Button } from "../../../lt-modules/Buttons";
-import BannerSlide from "../../../components/BannerSlider/BannerSlider";
-import { Icon } from "../../../components/Icon";
+// import BannerSlide from "../../../components/BannerSlider/BannerSlider";
+// import { Icon } from "../../../components/Icon";
 
-function SampleNextArrow(props) {
-  const { className, onClick } = props;
-  return (
-    <Icon
-      name="icon-slider-main-arrow"
-      className={className}
-      onClick={onClick}
-      size={"0px"}
-    />
-  );
-}
-function SamplePrevArrow(props) {
-  const { className, onClick } = props;
-  return (
-    <Icon
-      name="icon-slider-main-arrow"
-      className={className}
-      onClick={onClick}
-      size={"0px"}
-    />
-  );
-}
+// function SampleNextArrow(props) {
+//   const { className, onClick } = props;
+//   return (
+//     <Icon
+//       name="icon-slider-main-arrow"
+//       className={className}
+//       onClick={onClick}
+//       size={"0px"}
+//     />
+//   );
+// }
+// function SamplePrevArrow(props) {
+//   const { className, onClick } = props;
+//   return (
+//     <Icon
+//       name="icon-slider-main-arrow"
+//       className={className}
+//       onClick={onClick}
+//       size={"0px"}
+//     />
+//   );
+// }
 
-const settings = {
-  infinite: true,
-  speed: 500,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  arrows: true,
-  dotsClass: `slick-dots ${style.dots}`,
-  nextArrow: <SampleNextArrow />,
-  prevArrow: <SamplePrevArrow />,
-  autoplay: true,
-  autoplaySpeed: 3000,
-  responsive: [
-    {
-      breakpoint: 743,
-      settings: {
-        dots: true,
-        arrows: false,
-      },
-    },
-  ],
-};
+// const settings = {
+//   infinite: true,
+//   speed: 500,
+//   slidesToShow: 1,
+//   slidesToScroll: 1,
+//   arrows: true,
+//   dotsClass: `slick-dots ${style.dots}`,
+//   nextArrow: <SampleNextArrow />,
+//   prevArrow: <SamplePrevArrow />,
+//   autoplay: true,
+//   autoplaySpeed: 3000,
+//   responsive: [
+//     {
+//       breakpoint: 743,
+//       settings: {
+//         dots: true,
+//         arrows: false,
+//       },
+//     },
+//   ],
+// };
 
 export default function NewMainMain(props) {
   return (
     <>
-      <Slider {...settings} className={style.customSlider}>
+      {/* <Slider {...settings} className={style.customSlider}>
         <BannerSlide
           logoImg="/blog/iaapa-expo-europe-2024/logo.png"
           spanTitle="Visit NETRONIC"
@@ -69,60 +69,60 @@ export default function NewMainMain(props) {
           stand="#1549"
           buttonText="Register Now"
           registerLink="https://netronic.net/amusement-expo-international-2025"
-        />
-        <section className={style.main}>
-          {props.image && (
-            <div className="background desktop">
-              <Image
-                src={props.image}
-                layout="fill"
-                objectFit="cover"
-                objectPosition={props.image_objectPosition}
-                quality={90}
-                priority={true}
-                alt="image"
-              />
-            </div>
-          )}
-          {props.image_responsive && (
-            <div className="background mobile">
-              <Image
-                src={props.image_responsive}
-                layout="fill"
-                objectFit={props.image_responsive_objectFit}
-                objectPosition={props.image_responsive_objectPosition}
-                quality={90}
-                priority={true}
-                alt="image"
-              />
-            </div>
-          )}
-          <div className={style.content}>
-            <h1 className={`${style.title} fade-right-animation`}>
-              {props.title}
-            </h1>
-            <p className={`${style.text} fade-right-animation`}>{props.text}</p>
-            <div className={`${style.buttons} fade-right-animation`}>
-              <Button
-                type="catalog"
-                text={props.buttonText}
-                style="blueBlackMainBannerWithGirl"
-              />
-              {props.eventButtonLink && (
-                <div>
-                  <a href={props.eventButtonLink}>
-                    <FillButton
-                      className={style.event_button}
-                      style="whiteBlack"
-                      text={props.eventButtonText}
-                    />
-                  </a>
-                </div>
-              )}
-            </div>
+        /> */}
+      <section className={style.main}>
+        {props.image && (
+          <div className="background desktop">
+            <Image
+              src={props.image}
+              layout="fill"
+              objectFit="cover"
+              objectPosition={props.image_objectPosition}
+              quality={90}
+              priority={true}
+              alt="image"
+            />
           </div>
-        </section>
-      </Slider>
+        )}
+        {props.image_responsive && (
+          <div className="background mobile">
+            <Image
+              src={props.image_responsive}
+              layout="fill"
+              objectFit={props.image_responsive_objectFit}
+              objectPosition={props.image_responsive_objectPosition}
+              quality={90}
+              priority={true}
+              alt="image"
+            />
+          </div>
+        )}
+        <div className={style.content}>
+          <h1 className={`${style.title} fade-right-animation`}>
+            {props.title}
+          </h1>
+          <p className={`${style.text} fade-right-animation`}>{props.text}</p>
+          <div className={`${style.buttons} fade-right-animation`}>
+            <Button
+              type="catalog"
+              text={props.buttonText}
+              style="blueBlackMainBannerWithGirl"
+            />
+            {props.eventButtonLink && (
+              <div>
+                <a href={props.eventButtonLink}>
+                  <FillButton
+                    className={style.event_button}
+                    style="whiteBlack"
+                    text={props.eventButtonText}
+                  />
+                </a>
+              </div>
+            )}
+          </div>
+        </div>
+      </section>
+      {/* </Slider> */}
     </>
   );
 }
