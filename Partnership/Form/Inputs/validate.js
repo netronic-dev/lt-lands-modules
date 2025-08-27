640afa27387b21f61d514467b523b36ebf84ddaf
Netronic Developer <115734128+netronic-dev@users.noreply.github.com>
2025-08-27 16:09:52 +0300

    add rule for vat validation in partner form

--- Partnership/Form/Inputs/validate.js
+++ Partnership/Form/Inputs/validate.js
@@ -1,57 +1,3 @@
-// import * as yup from "yup";
-// import {
-//   parsePhoneNumberFromString,
-//   isPossiblePhoneNumber,
-// } from "libphonenumber-js";
-// import BadWordsNext from "bad-words-next";
-// import en from "bad-words-next/data/en.json";
-// import ru from "bad-words-next/data/ru.json";
-// import uk from "bad-words-next/data/ua.json";
-// import rl from "bad-words-next/data/ru_lat.json";
-
-// const phoneNumberValidator = (value) => {
-//   if (!value) return false;
-
-//   const phoneNumber = parsePhoneNumberFromString(`+${value}`);
-//   return phoneNumber ? phoneNumber.isValid() : false;
-// };
-
-// const possiblePhoneNumberValidator = (value) => {
-//   if (!value) return false;
-
-//   return isPossiblePhoneNumber(`+${value}`);
-// };
-
-// const badWordsValidator = (value) => {
-//   const badwords = new BadWordsNext();
-//   badwords.add(en);
-//   badwords.add(ru);
-//   badwords.add(uk);
-//   badwords.add(rl);
-
-//   return !badwords.check(value);
-// };
-
-// export const schema = yup
-//   .object({
-//     name: yup
-//       .string()
-//       .required("Name is required")
-//       .test("no-profanity", "Name contains swear words.", badWordsValidator),
-//     email: yup.string().required("Email is required").email("Invalid email"),
-//     phoneNumber: yup
-//       .string()
-//       .required("Phone number is required")
-//       .test("is-valid-phone", "Phone number is not valid", phoneNumberValidator)
-//       .test(
-//         "is-possible-phone",
-//         "Phone number is not possible",
-//         possiblePhoneNumberValidator
-//       ),
-//     agreement: yup.boolean().oneOf([true], "Required").required(),
-//   })
-//   .required();
-
 import * as yup from "yup";
 import {
   parsePhoneNumberFromString,
@@ -88,19 +34,22 @@ export const schema = yup
     // 1. Company details
     companyLegalName: yup
       .string()
+      .trim()
       .required("Company Legal Name is required")
       .test("no-profanity", "Invalid words detected", badWordsValidator),
     businessAddress: yup
       .string()
+      .trim()
       .required("Registered Business Address is required"),
-    country: yup.string().required("Country is required"),
-    city: yup.string().required("City is required"),
-    vatNumber: yup.string().required("VAT / Tax ID is required"),
+    country: yup.string().trim().required("Country is required"),
+    city: yup.string().trim().required("City is required"),
+    vatNumber: yup.string().trim().required("VAT / Tax ID is required"),
     website: yup.string().url("Invalid website URL").nullable(),
 
     // 2. Contact info
     firstName: yup
       .string()
+      .trim()
       .required("First name is required")
       .test(
         "no-profanity",
@@ -109,10 +58,12 @@ export const schema = yup
       ),
     lastName: yup
       .string()
+      .trim()
       .required("Last name is required")
       .test("no-profanity", "Last name swear words", badWordsValidator),
     position: yup
       .string()
+      .trim()
       .required("Position is required")
       .test("no-profanity", "Invalid words detected", badWordsValidator),
     phoneNumber: yup
@@ -124,7 +75,11 @@ export const schema = yup
         "Phone number is not possible",
         possiblePhoneNumberValidator
       ),
-    email: yup.string().required("Email is required").email("Invalid email"),
+    email: yup
+      .string()
+      .trim()
+      .required("Email is required")
+      .email("Invalid email"),
 
     // 3. Company type & Motivation
     companyType: yup
